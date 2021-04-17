76d11ffeaee5aec3cb77dd90ec18be58487d5b88
devyash17 <devyash17@gmail.com>
2021-04-17 15:22:28 +0530

    initial commit

---
+++ src/main/java/com/example/entropy/EntropyApplication.java
@@ -0,0 +1,13 @@
+package com.example.entropy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntropyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntropyApplication.class, args);
+	}
+
+}
